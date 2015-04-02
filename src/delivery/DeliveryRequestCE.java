307c89db075ded647c4b2068f7c2c28f61908d4f
sedog <sedog@SFS-PC-02.sfs.uncc.edu>
2015-04-01 20:48:44 -0400

    Added common jar to libraries and worked on AbstractGopher and DeliveryRequestGopher

---
+++ src/delivery/DeliveryRequestCE.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delivery;
+
+import common.DeliveryRequest;
+import common.Status;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author sedog
+ */
+public class DeliveryRequestCE implements DeliveryRequest, Serializable {
+    private static final long serialVersionUID = 123L;
+    private int bidID;
+    private int customerID;
+    private int deliveryRequestID;
+    private String description;
+    private String dropOffAddress;
+    private Timestamp dropOffTime;
+    private String pickUpAddress;
+    private Timestamp pickUpTime;
+    private Timestamp postTime;
+    private Timestamp realDropOffTime;
+    private Timestamp realPickUpTime;
+    private Status status;
+    private float weight;
+
+    /**
+     * @return the bidID
+     */
+    public int getBidID() {
+        return bidID;
+    }
+
+    /**
+     * @param bidID the bidID to set
+     */
+    public void setBidID(int bidID) {
+        this.bidID = bidID;
+    }
+
+    /**
+     * @return the customerID
+     */
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    /**
+     * @param customerID the customerID to set
+     */
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    /**
+     * @return the deliveryRequestID
+     */
+    public int getDeliveryRequestID() {
+        return deliveryRequestID;
+    }
+
+    /**
+     * @param deliveryRequestID the deliveryRequestID to set
+     */
+    public void setDeliveryRequestID(int deliveryRequestID) {
+        this.deliveryRequestID = deliveryRequestID;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the dropOffAddress
+     */
+    public String getDropOffAddress() {
+        return dropOffAddress;
+    }
+
+    /**
+     * @param dropOffAddress the dropOffAddress to set
+     */
+    public void setDropOffAddress(String dropOffAddress) {
+        this.dropOffAddress = dropOffAddress;
+    }
+
+    /**
+     * @return the dropOffTime
+     */
+    public Timestamp getDropOffTime() {
+        return dropOffTime;
+    }
+
+    /**
+     * @param dropOffTime the dropOffTime to set
+     */
+    public void setDropOffTime(Timestamp dropOffTime) {
+        this.dropOffTime = dropOffTime;
+    }
+
+    /**
+     * @return the pickUpAddress
+     */
+    public String getPickUpAddress() {
+        return pickUpAddress;
+    }
+
+    /**
+     * @param pickUpAddress the pickUpAddress to set
+     */
+    public void setPickUpAddress(String pickUpAddress) {
+        this.pickUpAddress = pickUpAddress;
+    }
+
+    /**
+     * @return the postTime
+     */
+    public Timestamp getPostTime() {
+        return postTime;
+    }
+
+    /**
+     * @param postTime the postTime to set
+     */
+    public void setPostTime(Timestamp postTime) {
+        this.postTime = postTime;
+    }
+
+    /**
+     * @return the realDropOffTime
+     */
+    public Timestamp getRealDropOffTime() {
+        return realDropOffTime;
+    }
+
+    /**
+     * @param realDropOffTime the realDropOffTime to set
+     */
+    public void setRealDropOffTime(Timestamp realDropOffTime) {
+        this.realDropOffTime = realDropOffTime;
+    }
+
+    /**
+     * @return the realPickUpTime
+     */
+    public Timestamp getRealPickUpTime() {
+        return realPickUpTime;
+    }
+
+    /**
+     * @param realPickUpTime the realPickUpTime to set
+     */
+    public void setRealPickUpTime(Timestamp realPickUpTime) {
+        this.realPickUpTime = realPickUpTime;
+    }
+    
+    /**
+     * @return the status
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the weight
+     */
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the pickUpTime
+     */
+    public Timestamp getPickUpTime() {
+        return pickUpTime;
+    }
+
+    /**
+     * @param pickUpTime the pickUpTime to set
+     */
+    public void setPickUpTime(Timestamp pickUpTime) {
+        this.pickUpTime = pickUpTime;
+    }
+}
---
+++ src/delivery/DeliveryRequestGopher.java
@@ -0,0 +1,95 @@
+package delivery;
+
+import common.DeliveryRequest;
+import common.Status;
+import gopher.AbstractGopher;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class handles the creation and processing of database queries related to
+ * <code>DeliveryRequest</code> objects.
+ * 
+ * @author sedog
+ */
+public class DeliveryRequestGopher extends AbstractGopher {
+    // Indeces for each database column so thtey only need to be set once
+    public final static String TABLE_NAME = "delivery_request";
+    public final static String BID_ID = "bid_id";
+    public final static String CUST_ID = "customer_id";
+    public final static String REQ_ID = "delivery_request_id";
+    public final static String DESCR = "description";
+    public final static String DROP_ADDR = "drop_off_address";
+    public final static String DROP_TIME = "drop_off_time";
+    public final static String PICKUP_ADDR = "pick_up_address";
+    public final static String PICKUP_TIME = "pick_up_time";
+    public final static String REAL_DROP_TIME = "real_drop_off_time";
+    public final static String REAL_PICKUP_TIME = "real_pick_up_time";
+    public final static String STATUS = "status";
+    public final static String WEIGHT = "weight";
+    
+    @Override
+    protected DeliveryRequest parseResult(ResultSet results) {
+        DeliveryRequest request = new DeliveryRequestCE();
+        
+        try {
+            request.setBidID(results.getInt(BID_ID));
+            request.setCustomerID(results.getInt(CUST_ID));
+            request.setDeliveryRequestID(results.getInt(REQ_ID));
+            request.setDescription(results.getString(DESCR));
+            request.setDropOffAddress(results.getString(DROP_ADDR));
+            request.setDropOffTime(results.getTimestamp(DROP_TIME));
+            request.setPickUpAddress(results.getString(PICKUP_ADDR));
+            request.setPickUpTime(results.getTimestamp(PICKUP_TIME));
+            request.setRealDropOffTime(results.getTimestamp(REAL_DROP_TIME));
+            request.setRealPickUpTime(results.getTimestamp(REAL_PICKUP_TIME));
+            request.setStatus(Status.map(results.getInt(STATUS)));
+            request.setWeight(results.getFloat(WEIGHT));
+        } catch (SQLException ex) {
+            System.out.println("Unable to read from resultSet");
+            ex.printStackTrace();
+        }
+        
+        return request;
+    }
+    
+    /**
+     * Retrieves and returns the <code>DeliveryRequest</code> identified by the
+     * given primary key
+     * 
+     * @param id    the primary key of the record to retrieve
+     * @return      a <code>DeliveryRequest</code> populated with data retrieved
+     *              from the database
+     */
+    public DeliveryRequest get(int id) {
+        // The query to execute
+        String query = "SELECT * FROM " + TABLE_NAME + "WHERE " + REQ_ID + "="
+                + id;
+        // The raw list of results
+        List<Object> rawList = null;
+        // The converted list of results
+        List<DeliveryRequest> castList = null;
+        // The DeliveryRequest to return
+        DeliveryRequest request = null;
+       
+        rawList = super.executeQuery(query);
+        castList = convert(rawList);
+        request = castList.get(0);
+        
+        return request;
+    }
+    
+    private List<DeliveryRequest> convert(List<Object> list) {
+        List<DeliveryRequest> converted = new ArrayList<DeliveryRequest>(
+                list.size());
+        for (Object each : list) {
+            if (each instanceof DeliveryRequest) {
+                converted.add((DeliveryRequest) each);
+            } 
+        }
+        
+        return converted;
+    }
+}
