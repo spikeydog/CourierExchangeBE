e97d066e1457da8fa7c1c446c8e31c5a5eec9f3d
sedog <sedog@SFS-PC-02.sfs.uncc.edu>
2015-04-15 10:43:46 -0400

    Added explicit testing class

---
+++ src/launch/Tester.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package launch;
+
+/**
+ * This is a freely-mutable tester class for anyone to use. 
+ * 
+ * When you write your own tests, simply do not commit any changes to the repo,
+ * so other team member's tests will not be adversely affected.
+ * 
+ * @author sedog
+ */
+public class Tester {
+    public final static void main(String[] args) {
+        System.out.println("Testing class initiated");
+    }
+}
